b8281945ca037df1c06a11e4cd315130683e0690
Manfred Hantschel <thred@users.noreply.github.com>
2015-05-15 08:02:02 +0200

    visual cosmetics

--- src/main/java/io/github/thred/climatetray/util/swing/BorderPanel.java
+++ src/main/java/io/github/thred/climatetray/util/swing/BorderPanel.java
@@ -1,31 +1,54 @@
 package io.github.thred.climatetray.util.swing;
 
 import java.awt.BorderLayout;
+import java.awt.LayoutManager;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class BorderPanel extends JPanel
 {
 
+    private static final Border DEFAULT_BORDER = BorderFactory.createEmptyBorder(8, 12, 8, 12);
     private static final long serialVersionUID = -3653014923804516104L;
 
-    public BorderPanel()
+    public BorderPanel(LayoutManager layoutManager)
     {
-        this(null);
+        this(layoutManager, DEFAULT_BORDER, null);
+    }
+
+    public BorderPanel(LayoutManager layoutManager, Border border)
+    {
+        this(layoutManager, border, null);
     }
 
     public BorderPanel(JComponent component)
     {
-        super(new BorderLayout());
+        this(new BorderLayout(), DEFAULT_BORDER, component);
+    }
+
+    public BorderPanel(Border border, JComponent component)
+    {
+        this(new BorderLayout(), border, component);
+    }
+
+    protected BorderPanel(LayoutManager layoutManager, Border border, JComponent component)
+    {
+        super(layoutManager);
 
         setOpaque(false);
-        setBorder(BorderFactory.createEmptyBorder(8, 12, 8, 12));
+
+        if (border != null)
+        {
+            setBorder(border);
+        }
 
         if (component != null)
         {
             add(component, BorderLayout.CENTER);
         }
     }
+
 }
