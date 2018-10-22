e76232e2231ae9fb0b99a5c415d208ce912a2eba
Manfred Hantschel <thred@users.noreply.github.com>
2018-10-22 20:03:27 +0200

    Update gh-pages

--- js/climate-tray.js
+++ js/climate-tray.js
@@ -1,9 +1,19 @@
+var platform = navigator.platform.toLowerCase();
+var extension;
+
+if (platform.indexOf("linux") >= 0) {
+    extension = "-bin.tar.gz";
+}
+else {
+    extension = "-bin.zip";
+}
+
 $.ajax({
     url: "VERSION",
     success: function (data) {
         var version = data.match(/build\.version\s*=\s*([^\n]*)/)[1];
 
-        $("#download-button").attr("href", "climate-tray-" + version + ".zip");
+        $("#download-button").attr("href", "climate-tray-" + version + extension);
         $("#version").html(version);
     }
 });
